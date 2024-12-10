b7d2340e6b9d6c4ae153438db036b08ce530be4d
Hermi-git <andargiehermela1@gmail.com>
2024-12-10 12:21:27 +0300

    Categories

---
+++ amazon/src/components/Category/CategoryFullInfo.js
@@ -0,0 +1,21 @@
+export const categoryInfos =[ {
+    title: "Electronics",
+    name: "electronics",
+    imgLink: "https://img.freepik.com/free-photo/modern-stationary-collection-arrangement_23-2149309643.jpg"
+},
+ {
+    title: "Discover Fashion trends",
+    name: "Women's clothing",
+    imgLink: "https://nypost.com/wp-content/uploads/sites/2/2024/01/workwear.jpg?quality=75&strip=all"
+ }, 
+ {
+    title: "Men's clothing",
+    name: "men's clothing",
+    imgLink: "https://teeshoppen.be/cdn/shop/files/the-original-performance-suit-burgundy-shirt-tie-derby-shoes-package-deal-488991_1024x.jpg?v=1726274490"
+ },
+ {
+    title: "Jewelery",
+    name: "jewelery",
+    imgLink: "https://gauravjewellers.com/wp-content/uploads/2024/04/Gold-Necklace-.jpg"
+ }
+]
